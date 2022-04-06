efd4aaf2b3e0a7ef370752118856054b98c9db88
Roksana <roksana.rebiewska@gmail.com>
2022-04-06 20:55:50 +0200

    Modal portal implementation

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,14 +1,21 @@
+import ReactDOM from 'react-dom';
+
 import classes from './Modal.module.scss';
 
 const Modal = ({ onConfirm, onClose }) => {
   return (
     <>
-      <div className={classes.backdrop} onClick={onClose}></div>
-      <div className={classes.modal}>
-        <p>Are you sure you want to delete?</p>
-        <button onClick={onConfirm}>Delete</button>
-        <button onClick={onClose}>Close</button>
-      </div>
+      {ReactDOM.createPortal(
+        <>
+          <div className={classes.backdrop} onClick={onClose}></div>
+          <div className={classes.modal}>
+            <p>Are you sure you want to delete?</p>
+            <button onClick={onConfirm}>Delete</button>
+            <button onClick={onClose}>Close</button>
+          </div>
+        </>,
+        document.getElementById('modal-root')
+      )}
     </>
   );
 };
